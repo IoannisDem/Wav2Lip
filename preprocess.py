04b4ddfdf0a28f5249f16bfd04c6f4ea22f1e23f
IoannisDem <ioannisdem2000@gmail.com>
2024-11-04 21:51:44 +0000

    update

--- preprocess.py
+++ preprocess.py
@@ -20,14 +20,14 @@ from hparams import hparams as hp
 
 import face_detection
 
-parser = argparse.ArgumentParser()
+# parser = argparse.ArgumentParser()
 
-parser.add_argument('--ngpu', help='Number of GPUs across which to run in parallel', default=1, type=int)
-parser.add_argument('--batch_size', help='Single GPU Face detection batch size', default=32, type=int)
-parser.add_argument("--data_root", help="Root folder of the LRS2 dataset", required=True)
-parser.add_argument("--preprocessed_root", help="Root folder of the preprocessed dataset", required=True)
+# parser.add_argument('--ngpu', help='Number of GPUs across which to run in parallel', default=1, type=int)
+# parser.add_argument('--batch_size', help='Single GPU Face detection batch size', default=32, type=int)
+# parser.add_argument("--data_root", help="Root folder of the LRS2 dataset", required=True)
+# parser.add_argument("--preprocessed_root", help="Root folder of the preprocessed dataset", required=True)
 
-args = parser.parse_args()
+# args = parser.parse_args()
 
 fa = [face_detection.FaceAlignment(face_detection.LandmarksType._2D, flip_input=False, 
 									device='cuda:{}'.format(id)) for id in range(args.ngpu)]
